53f64d63bf55bd4b29eab03a43755268db70f523
Matthew <jmatthewpareja@gmail.com>
2025-04-10 20:37:19 +0200

    Tdood

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,43 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
+import {
+  MatButtonToggle,
+  MatButtonToggleGroup,
+} from '@angular/material/button-toggle';
+import { MatIconModule } from '@angular/material/icon';
+import { DragDropModule } from '@angular/cdk/drag-drop';
+import { AppointmentDialogComponent } from './appointment-dialog/appointment-dialog.component';
+import {CalendarComponent} from './calendar/calendar.component';
+import {ReactiveFormsModule} from '@angular/forms';
+import {MatFormField, MatLabel} from '@angular/material/input';
+import {MatDatepicker, MatDatepickerInput, MatDatepickerToggle} from '@angular/material/datepicker';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CalendarComponent,
+    AppointmentDialogComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CommonModule,
+    MatButtonModule,
+    MatButtonToggleGroup,
+    MatButtonToggle,
+    MatIconModule,
+    DragDropModule,
+    ReactiveFormsModule,
+    MatLabel,
+    MatFormField,
+    MatDatepickerInput,
+    MatDatepickerToggle,
+    MatDatepicker,
   ],
   providers: [],
   bootstrap: [AppComponent]
